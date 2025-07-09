fc71a533ab483fc0a80d1c9da04505571d25728c
Noah <nbelman1@gmail.com>
2025-07-08 21:45:00 -0400

    Create arrays, access and log arrays

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-code
+// Task 1: create arrays
+let groceryList = ["milk", "bananas", "turkey", "sliced cheese", "avocados"];
+let priceList = [10.99, 5.49, 7.74];
+
+// Task 2: access and log the arrays
+console.log(groceryList);
+console.log(priceList);
+console.log(groceryList[0], groceryList[1], groceryList[4]);
+console.log(priceList[1], priceList[2]);
